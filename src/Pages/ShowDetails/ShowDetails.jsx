b20fe2aa0ee4b13a181d2cd117f3694c5daff685
nurullah91 <mdnurullah.ptk@gmail.com>
2024-02-01 04:54:56 +0600

    details card designed

--- src/Pages/ShowDetails/ShowDetails.jsx
+++ src/Pages/ShowDetails/ShowDetails.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import DOMPurify from 'dompurify';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PropagateLoader } from "react-spinners";
@@ -15,32 +16,50 @@ const ShowDetails = () => {
             .catch(err => console.log(err))
     }, [id])
 
+    const { name, runtime, premiered, language, summary, image, genres } = showData;
 
-
-    const { name, runtime, premiered, type, url, language, summary, image, genres } = showData;
-    console.log({ name, runtime, premiered, type, url, language, summary, image, genres })
-
-    console.log(showData);
+    // sanitize data for safely render HTML
+    const sanitizedSummery = DOMPurify.sanitize(summary);
 
 
     return (
-        name ? <section className="w-11/12 md:w-9/12 mx-auto my-10">
-            {/*Section Header */}
-            <div className="flex gap-5">
-                <img className="w-[100px]" src={image.medium} alt="image" />
-                <div>
-                    <h3 className="text-2xl font-semibold text-slate-100">{name}</h3>
-                    {premiered && <p className="text-yellow-400">Premiered: {premiered}</p>}
+        name ? <section className="w-11/12 md:w-9/12 mx-auto my-10 myShadow">
+            {/* Card */}
+            <div className="px-5 md:px-10 py-10">
 
-                    <div>
-                        {runtime && <button className="border border-slate-400 rounded px-4 py-1 mr-4 my-3 text-white">{runtime} min</button>}
-                        {genres.map((item, index) => <span key={index} className="mr-3 text-green-300 border-r-2 border-green-800 px-2">{item}</span>)}
+                <div className="lg:flex">
+                    {/* Show info */}
+                    <div className="lg:w-1/2">
+                        {/*Section Header */}
+                        <div className="flex gap-5">
+                            <img className="w-[100px]" src={image.medium} alt="image" />
+                            <div>
+                                <h3 className="text-2xl font-semibold text-slate-100">{name}</h3>
+                                {premiered && <p className="text-yellow-400">Premiered: {premiered}</p>}
+
+                                {language && <p className="text-slate-400">Language: {language}</p>}
+
+                                <div>
+                                    {runtime && <button className="border border-slate-400 rounded px-4 py-1 mr-4 my-3 text-white">{runtime} min</button>}
+                                    {genres.map((item, index) => <span key={index} className="mr-3 text-green-300 border-r-2 border-green-800 px-2">{item}</span>)}
+                                </div>
+                            </div>
+                        </div>
+
+                        {summary && <div className="text-slate-300 my-5 ">
+                            <p dangerouslySetInnerHTML={{ __html: sanitizedSummery }} />
+                        </div>}
+
+                    </div>
+
+                    {/* Show image */}
+                    <div className="lg:w-1/3 mx-auto">
+                        {image.original && <img src={image.original} className="w-full" alt="image" />}
                     </div>
                 </div>
+
+
             </div>
-
-            {summary && <p className="text-slate-300 mt-5">{summary}</p>}
-
 
 
         </section> :
